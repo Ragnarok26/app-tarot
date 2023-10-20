79c27270c9e0351dad05d3ac5dda348a0188cf18
Carlos Iván Montes Agüero <ci.montes@hotmail.com>
2023-10-20 11:28:07 -0600

    Update CardItem.js

--- components/cards/CardItem/CardItem.js
+++ components/cards/CardItem/CardItem.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import React from 'react';
 import {useState} from 'react';
 
-const CardItem = ({ link, labels, props, tematic, qtyCards }) => {
+const CardItem = ({ link, labels, props, tematic, qtyCards, tematicFlag }) => {
   tematic = +tematic
   const isShowInterpretations = props.interpretation && [1000].includes(tematic)
   const isShowExtras = [2000].includes(tematic)
@@ -20,6 +20,7 @@ const CardItem = ({ link, labels, props, tematic, qtyCards }) => {
     6000: 'Salud',
     7000: 'Dinero',
   };
+
   const description = props.tematico.find(e => e.name === f[tematic])?.description || props.significado;
   const description_y_representation = props.description_y_representation;
   const carta_nacimiento_description = props.carta_nacimiento_description;
@@ -45,7 +46,7 @@ const CardItem = ({ link, labels, props, tematic, qtyCards }) => {
           </div>
 
           {
-            props.hasOwnProperty('tags')
+            props.hasOwnProperty('tags') && tematicFlag == true
             ? props.tags.length > 0
               ? <div style={{display: 'flex', justifyContent: 'center'}}>
                   <TagsDescription tags={props.tags}/>
@@ -55,8 +56,8 @@ const CardItem = ({ link, labels, props, tematic, qtyCards }) => {
           }
 
           {
-            props.hasOwnProperty('synthesis')
-            ?<p className={s.labelBackground} dangerouslySetInnerHTML={{ __html: props.synthesis }}></p>
+            props.hasOwnProperty('synthesis') && tematicFlag == true
+            ? <p className={s.labelBackground} dangerouslySetInnerHTML={{ __html: props.synthesis }}></p>
             :null
           }
           {/* <p className={s.label} dangerouslySetInnerHTML={{ __html: props.simbologia }}></p> */}
@@ -68,14 +69,15 @@ const CardItem = ({ link, labels, props, tematic, qtyCards }) => {
             </div>
           }
 
-            {/* <p className={s.description} dangerouslySetInnerHTML={{ __html: description }}></p> */}
-
-          <ReadMore descr={description} limit={170} readLess={labels.readLess} readMore={labels.readMore}/>
+          {
+            <ReadMore descr={description} styleClass={s.description} limit={170} readLess={labels.readLess} readMore={labels.readMore} flag={tematicFlag}/>
+            // <p className={s.description} dangerouslySetInnerHTML={{ __html: description }}></p>
+          }
 
           {
             isShowBirthCardExtraContent && (
+              <ReadMore descr={carta_nacimiento_description} styleClass={s.description} limit={170} readLess={labels.readLess} readMore={labels.readMore} flag={tematicFlag}/>
               // <p className={s.description} dangerouslySetInnerHTML={{ __html: carta_nacimiento_description }}></p>
-              <ReadMore descr={carta_nacimiento_description} limit={170} readLess={labels.readLess} readMore={labels.readMore}/>
             )
           }
 
@@ -84,7 +86,7 @@ const CardItem = ({ link, labels, props, tematic, qtyCards }) => {
             <div>
               <h3 className={s.inverseCard}>{labels.inverseDescriptionTitle}</h3>
               {/* <p className={s.description} dangerouslySetInnerHTML={{ __html: description_y_representation }}></p> */}
-              <ReadMore descr={description_y_representation} limit={170} readLess={labels.readLess} readMore={labels.readMore}/>
+              <ReadMore descr={description_y_representation} styleClass={s.description} limit={170} readLess={labels.readLess} readMore={labels.readMore} flag={tematicFlag}/>
             </div>
           }
 
@@ -121,10 +123,11 @@ const CardItem = ({ link, labels, props, tematic, qtyCards }) => {
                     src={ `/card/ic_ar_m${props.id}.png?q=100` }
                   />
                 </div>
-                <p className={s.label}>{props.inversa_simbologia}</p>
+                <ReadMore descr={props.inversa_simbologia} styleClass={s.label} limit={170} readLess={labels.readLess} readMore={labels.readMore} flag={tematicFlag}/>
+                {/* <p className={s.label}>{props.inversa_simbologia}</p> */}
                 <h3 className={s.inverseCard}>{labels.inverseSignificado}</h3>
+                <ReadMore descr={props.inversa_significado} styleClass={s.description} limit={170} readLess={labels.readLess} readMore={labels.readMore} flag={tematicFlag}/>
                 {/* <p className={s.description}>{props.inversa_significado}</p> */}
-                <ReadMore descr={props.inversa_significado} limit={170} readLess={labels.readLess} readMore={labels.readMore}/>
             </div>
           }
         </div>
@@ -160,49 +163,63 @@ const CardAssociations = ({ associations }) => {
   )
 }
 
-const ReadMore = ({ descr, limit, readLess, readMore }) => {
+const ReadMore = ({ descr, styleClass, limit, readLess, readMore, flag }) => {
   const [showAll, setShowAll] = useState(false);
-  return (
-    <span>
-      {descr.length > limit ? (
-        <>
-          {showAll ? (
-            <div className={s.description}>
-              <p className={s.description} dangerouslySetInnerHTML={{ __html: descr }}></p>
-              <a
-                onClick={() => setShowAll(false)}
-                className={s.textPrimary}
-              >
-                {readLess}
-              </a>
-            </div>
-          ) : (
-            <div className={s.description}>
-              <p className={s.description} dangerouslySetInnerHTML={{ __html: descr.substring(0, limit).concat("...") }}></p>
-              <a onClick={() => setShowAll(true)} className={s.textPrimary}>
-              {readMore}
-              </a>
-            </div>
-          )}
-        </>
-      ) : (
-        descr
-      )}
-    </span>
-  );
+  if(flag==true){
+    return (
+      <span>
+        {descr.length > limit ? (
+          <>
+            {showAll ? (
+              <div className={s.description}>
+                <p className={styleClass} dangerouslySetInnerHTML={{ __html: descr }}></p>
+                <a
+                  onClick={() => setShowAll(false)}
+                  className={s.textPrimary}
+                >
+                  {readLess}
+                </a>
+              </div>
+            ) : (
+              <div className={s.description}>
+                <p className={styleClass} dangerouslySetInnerHTML={{ __html: descr.substring(0, limit).concat("...") }}></p>
+                <a onClick={() => setShowAll(true)} className={s.textPrimary}>
+                {readMore}
+                </a>
+              </div>
+            )}
+          </>
+        ) : (
+          descr
+        )}
+      </span>
+    );
+  }else{
+    return(
+      <p className={styleClass} dangerouslySetInnerHTML={{ __html: descr }}></p>
+    );
+  }
 };
 
 const TagsDescription = ({tags}) =>{
+  // tags.sort(function (a, b) {
+  //   return b.length - a.length;
+  // });
+
   return(
-    <div style={{width: '270px', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }} className='container'>
-      {
-        tags.map((item) =>{
-          return <div className={s.tagsDisplay} >
-              <span style={{fontSize: '12px', padding: '0 3px 0 3px'}}>{item}</span>
-          </div>
-        })
-      }
-    </div>
+    <div style={{width:'55%'}}>
+      <p style={{textAlign: 'justify'}}>
+            {/* // <div style={{width: '77%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '4px'}} className='container'> */}
+            {
+              tags.map((item) =>{
+                // return <div className={s.tagsDisplay} >
+                //     <span style={{fontSize: '12px', padding: '0 8px 0 8px'}}>{item}</span>
+                // </div>
+                return <span><span className={s.tagsDisplay}>{item}</span> </span>
+              })
+            }
+            </p>
+        </div>
   );
 };
 
